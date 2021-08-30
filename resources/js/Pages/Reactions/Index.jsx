f9b397987beffa8a87c89eb7f0eca49adc9c0cc3
George Kelly <george.kelly@ukfast.co.uk>
2021-08-30 14:18:57 +0100

    Setup lazy image loading

--- resources/js/Pages/Reactions/Index.jsx
+++ resources/js/Pages/Reactions/Index.jsx
@@ -23,7 +23,7 @@ const copyToClipboard = reaction => async () => {
 
 const Reaction = reaction => <div className="p-3 m-3 cursor-pointer hover:bg-yellow-100" key={reaction.id} onClick={copyToClipboard(reaction)}>
     <div className="text-center w-full">
-        <img src={"https://" + reaction.url} alt="" className="mx-auto" style={{maxWidth: 200, minWidth: 100}} />
+        <img src={"https://" + reaction.url} alt="" loading="lazy" className="mx-auto" style={{maxWidth: 200, minWidth: 100}} />
     </div>
     {reaction.name}
 </div>
